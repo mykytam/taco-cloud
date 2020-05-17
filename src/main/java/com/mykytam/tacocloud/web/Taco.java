d9d52ebda94b2e6d8956b06bd4294750a357bc91
mykytam <morar.nikita@gmail.com>
2020-05-17 15:33:57 +0300

    Taco class created

---
+++ src/main/java/com/mykytam/tacocloud/web/Taco.java
@@ -0,0 +1,11 @@
+package com.mykytam.tacocloud.web;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+}
