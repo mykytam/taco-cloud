42e3ca4aa75f63a2d870864f139cb6ab7e842afb
mykytam <morar.nikita@gmail.com>
2020-05-29 17:13:24 +0300

    ProcessOrder method modified

--- src/main/java/com/mykytam/tacocloud/web/OrderController.java
+++ src/main/java/com/mykytam/tacocloud/web/OrderController.java
@@ -1,7 +1,9 @@
 package com.mykytam.tacocloud.web;
 
 import com.mykytam.tacocloud.Order;
+import com.mykytam.tacocloud.User;
 import com.mykytam.tacocloud.data.OrderRepository;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,14 +31,15 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid Order order, Errors errors,
+                               SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
-
+        order.setUser(user);
         orderRepo.save(order);
         sessionStatus.setComplete();
-
         return "redirect:/";
     }
 
