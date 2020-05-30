8df0364de0e4e1d54a57141938e1bcde4f0022ff
mykytam <morar.nikita@gmail.com>
2020-05-30 15:55:23 +0300

    Adding pagination using properties file

--- src/main/java/com/mykytam/tacocloud/data/OrderRepository.java
+++ src/main/java/com/mykytam/tacocloud/data/OrderRepository.java
@@ -1,7 +1,13 @@
 package com.mykytam.tacocloud.data;
 
 import com.mykytam.tacocloud.Order;
+import com.mykytam.tacocloud.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    List<Order> findByUserOrderByPlacedAtDesc(
+            User user, Pageable pageable);
 }
